33f73251322605a99dddfcddc0b8405d15cd38e1
oscarvel821 <120507347+oscarvel821@users.noreply.github.com>
2023-04-09 22:11:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import streamlit as st
 from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-# x = st.slider('Select a value')
-# st.write(x, 'squared is', x * x)
-
 model_name = "distilbert-base-uncased-finetuned-sst-2-english"
 
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
@@ -22,8 +19,3 @@ if text:
   out = classifier(text)
   st.json(out)
   
-# tokens = tokenizer.tokenize("We are very happy to show you the transformers library.")
-# token_ids = tokenizer.convert_token_to_ids(tokens)
-# input_ids = tokenizer("We are very happy to show you the transformers library.")
-
-# print(f'  tokens:{tokens}')
