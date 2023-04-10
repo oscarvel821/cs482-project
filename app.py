9ef9af0ac4582c74cba8cee2e840c909197ae350
oscarvel821 <120507347+oscarvel821@users.noreply.github.com>
2023-04-09 21:39:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import streamlit as st
+from transformers import pipeline
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+# x = st.slider('Select a value')
+# st.write(x, 'squared is', x * x)
+
+model_name = "distilbert-base-uncased-finetuned-sst-2-english"
+
+classifier = pipeline("sentiment-analysis", model=model_name)
+result = classifier(["We are very happy to show you the transformers library.", "We hope you dont hate it"])
+
+for result in results:
+  print(result)
