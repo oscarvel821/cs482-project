107823784284854fa25793ff6883f6ba08e42a1f
oscarvel821 <120507347+oscarvel821@users.noreply.github.com>
2023-04-09 22:11:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
 import streamlit as st
+from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+model_name = "distilbert-base-uncased-finetuned-sst-2-english"
+
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+results = classifier(["We are very happy to show you the transformers library.", "We hope you dont hate it"])
+
+for result in results:
+  print(result)
+  
+text = st.text_area("enter some text")
+
+if text:
+  out = classifier(text)
+  st.json(out)
+  
