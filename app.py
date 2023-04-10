a847dc94a82eccbfed2bb7d01a5a896750cbcd0a
oscarvel821 <120507347+oscarvel821@users.noreply.github.com>
2023-04-09 21:49:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from transformers import pipeline
 model_name = "distilbert-base-uncased-finetuned-sst-2-english"
 
 classifier = pipeline("sentiment-analysis", model=model_name)
-result = classifier(["We are very happy to show you the transformers library.", "We hope you dont hate it"])
+results = classifier(["We are very happy to show you the transformers library.", "We hope you dont hate it"])
 
 for result in results:
   print(result)
